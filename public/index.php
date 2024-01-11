96d8bed99c75606815d0c2788a6d3523e5b31108
Mudasar Ali <mudasarali88@gmail.com>
2024-01-12 00:17:59 +0500

    headers added for cors

--- public/index.php
+++ public/index.php
@@ -3,9 +3,9 @@
 use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
-header('Access-Control-Allow-Origin', '*');
-header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+// header('Access-Control-Allow-Origin', '*');
+// header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+// header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
 define('LARAVEL_START', microtime(true));
 
